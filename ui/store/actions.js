0ae06c21496582ec49ef6cec4a07d26774e60a76
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-11-01 14:38:21 +0400

    Check that objects are defined before using them
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -1037,7 +1037,7 @@ export function unlockSucceeded(message) {
 
 export function updateMetamaskState(newState) {
   return (dispatch, getState) => {
-    const { metamask: currentState } = getState();
+    const { metamask: currentState, appState } = getState();
 
     const { currentLocale, selectedAddress, provider } = currentState;
     const {
@@ -1048,7 +1048,9 @@ export function updateMetamaskState(newState) {
 
     const { currentNetworkTxList } = getState().metamask;
     const { currentNetworkTxList: newNetworkTxList } = newState;
-    const { failedTransactionsToDisplay = {} } = getState().appState;
+    
+    const { failedTransactionsToDisplay = {} } = appState ? appState : {};
+
     if (currentLocale && newLocale && currentLocale !== newLocale) {
       dispatch(updateCurrentLocale(newLocale));
     }
@@ -1113,7 +1115,7 @@ export function updateMetamaskState(newState) {
     });
 
     // check that tx status has not changed and remove it from failed transactions if it has
-    const foundTx = currentNetworkTxList.find((currentTx, index) => {
+    const foundTx = currentNetworkTxList && currentNetworkTxList.find((currentTx, index) => {
       const newTx = newNetworkTxList[index];
       if (
         failedTransactionsToDisplay &&
